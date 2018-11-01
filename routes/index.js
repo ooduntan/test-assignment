129d1b484cc0b5186a1a1160b8c45cb33cb17b2b
Oduntan Oluwatobiloba Stephen <stephenodheavywater@gmail.com>
2018-11-01 20:33:14 +0100

    fix book name

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var users = require('./books');
+var books = require('./books');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Welcome' });
 });
 
-router.use('/books', users);
+router.use('/books', books);
 
 
 module.exports = router;
